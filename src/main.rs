c92cf77959a8fd6810005380a0850870cb227b1c
guidota <guido@tamborindeguy.com.ar>
2023-07-03 17:09:39 -0300

    feat: add live endpoint (#3)

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+use camera_reel_service::{run, Context};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+pub struct Arguments {
+    #[clap(short, long, default_value = "5000")]
+    pub port: u16,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let args = Arguments::parse();
+
+    let context = Context { port: args.port };
+
+    run(context).await
+}
